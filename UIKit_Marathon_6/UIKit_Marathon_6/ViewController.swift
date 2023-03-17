5fb226538b2b39ecc3400fd61e9d4741eb18060d
Aleksandr Zubov <alexanderzubov@icloud.com>
2023-03-17 09:18:09 +0100

    UIKit_Marathon_6_fix

--- UIKit_Marathon_6/UIKit_Marathon_6/ViewController.swift
+++ UIKit_Marathon_6/UIKit_Marathon_6/ViewController.swift
@@ -15,17 +15,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let gradientLayer:CAGradientLayer = CAGradientLayer()
-        gradientLayer.cornerRadius = 15
-        gradientLayer.frame.size = self.myView.frame.size
-        gradientLayer.colors = [UIColor.systemCyan.cgColor,UIColor.red.cgColor]
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
-
         myView.layer.cornerRadius = 10
         myView.clipsToBounds = true
-        myView.layer.masksToBounds = false
-        myView.layer.addSublayer(gradientLayer)
+        myView.layer.masksToBounds = true
+        myView.backgroundColor = .systemBlue
         
         view.addSubview(myView)
         
@@ -35,24 +28,26 @@ class ViewController: UIViewController {
     
     @objc func handleTap(_ sender: UITapGestureRecognizer) {
         let tapCoordinates = sender.location(in: view)
-        
+            
         let dx = tapCoordinates.x - myView.center.x
         let dy = tapCoordinates.y - myView.center.y
         let distance = abs(dx + dy)
-
+        let angleCoefficient = min(abs(dx + dy)/3000, 0.05)
+        
         let rotatingTime = min(distance/8000, 0.6)
         let transitionTime = CFTimeInterval(3)
-        
+    
         let springAnimation = CASpringAnimation(keyPath: "position")
         springAnimation.initialVelocity = 0
+        springAnimation.stiffness = 100.0
         springAnimation.fromValue = myView.layer.position
         springAnimation.damping = 10
-        springAnimation.mass = max(distance/1600, 1)
+        springAnimation.mass = min(distance/300, 0.6)
         springAnimation.toValue = tapCoordinates
         springAnimation.duration = transitionTime
         
-        let rotationTransformRight = CGAffineTransform(rotationAngle: CGFloat.pi / 10)
-        let rotationTransformLeft = CGAffineTransform(rotationAngle: -CGFloat.pi / 10)
+        let rotationTransformRight = CGAffineTransform(rotationAngle: CGFloat.pi * angleCoefficient)
+        let rotationTransformLeft = CGAffineTransform(rotationAngle: -CGFloat.pi * angleCoefficient)
         
         let movementAnimation: Void = UIView.animateKeyframes(withDuration: transitionTime, delay: 0.0,
             animations: {
